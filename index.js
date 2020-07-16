25c322b6d782b180bc0b6f3d50678bb41b953e11
h-a-s-a-n <16420149+h-a-s-a-n@users.noreply.github.com>
2020-07-16 05:57:11 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const defaultOutput = {
+  success: false,
+  errors: [],
+  message: null,
+};
+
+const messageBody = (input, options) => {
+  let output = input;
+  if (options.type === "code") {
+    output = "Log data:\n```\n" + input + "\n```";
+  } else if (options.type === "error") {
+    output = "Error:\n```\n" + input + "\n```";
+  }
+  console.log(output);
+  return output;
+};
+
+export const notify = async (input, options) => {
+  let output = defaultOutput;
+  //let output = JSON.parse(JSON.stringify(defaultOutput))
+  let message = {
+    text: "```\n" + input + "\n```",
+  };
+  message.text = messageBody(input, options);
+  try {
+    const res = await fetch(options.webhook, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(message, null, 2),
+    });
+    output.success = res.ok;
+  } catch (err) {
+    console.log(err);
+    output.errors.push(err.message);
+  }
+  return output;
+};
